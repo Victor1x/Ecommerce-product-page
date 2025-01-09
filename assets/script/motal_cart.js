5833c6d0730fb23b37e30bc67e27959bab0bdb34
João Victor <joaovictor160x@gmail.com>
2025-01-09 08:39:53 -0300

    terceiro commit

--- assets/script/motal_cart.js
+++ assets/script/motal_cart.js
@@ -1,6 +1,20 @@
+import { pedido, sempedido } from "./string.js";
+const modal_container = document.querySelector(".modal-container-cart");
+
+function openModal(message = sempedido) {
+  
+  modal_container.classList.add("active");
+  modal_container.addEventListener("click", (ev) => {
+    if (ev.target.className.indexOf("modal-container") !== -1) {
+      modal_container.classList.remove("active");
+    }
+  });
+}
+
 export function setupLinkCartEvent(link_cart) {
-  link_cart.addEventListener('click', (ev) => {
+  link_cart.addEventListener("click", (ev) => {
     ev.preventDefault(); // Impede o comportamento padrão do link
-    console.log('O link foi clicado!');
+    openModal(); // Chama a função corretamente
+    console.log("O link foi clicado!");
   });
 }
--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,10 @@ import {setupLinkCartEvent } from './motal_cart.js'
 
 const link_cart = document.querySelector(".link_cart")
 setupLinkCartEvent(link_cart)
+
+const toggle = document.querySelector('.navbar-toggle');
+const navLinks = document.querySelector('.navbar-links');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
